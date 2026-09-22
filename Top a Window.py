80f1d0289a0cc270b7b6e8b1c9e24754137c8b8b
919004-crypto <919004@pdsb.net>
2026-09-22 16:17:46 -0400

    Add files via upload

---
+++ Top a Window.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root=Tk()
+root.geometry("400x300")
+root.title("main")
+
+def topwin():
+    top = Toplevel()
+    top.geometry("400x300")
+    top.title("toplevel")
+
+    12 = Label(top, text="This is toplevel window")
+    12.pack()
+
+    top.mainloop
+
+l = Label(root, text="this is a root window")
+btn = Button(root, text="click here to open another window", command=topwin)
+
+l.pack()
+btn.pack()
+
+root.mainloop()
